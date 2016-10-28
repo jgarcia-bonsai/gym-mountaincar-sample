e9bc522f0ce04a430aee0c16556ad297349c1a03
John Garcia <john.garcia@bons.ai>
2016-10-27 21:41:48 -0700

    Remove gym-common components

--- gym_common/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-The bonsai.gym package contains the classes necessary for using OpenAI
-gym as a simulator for braind.
-"""
-
-from gym_common.gym_simulator import GymSimulator
-from gym_common.image_gym_simulator import ImageGymSimulator
