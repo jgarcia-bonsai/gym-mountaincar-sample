ea9dbcb8311dd241aad7a4d6acc4757498518631
John Garcia <john.garcia@bons.ai>
2016-11-19 22:19:25 -0800

    First big revision

--- mountaincar_simulator.py
+++ mountaincar_simulator.py
@@ -15,8 +15,9 @@ class MountainCarSimulator(GymSimulator):
             env, skip_frame=skip_frame,
             record_path=record_path, render_env=render_env)
 
+    @property
     def get_state(self):
-        state_schema = super().get_state()
+        state_schema = super().get_state
         return {"x_position": state_schema[0],
                 "x_velocity": state_schema[1]}
 
@@ -25,5 +26,6 @@ if __name__ == "__main__":
     base_args = bonsai.parse_base_arguments()
     simulator = MountainCarSimulator(
         env, SKIPPED_FRAME, RECORD_PATH, not base_args.headless)
+    assert isinstance(base_args, object)
     bonsai.run_with_url("mountaincar_simulator", simulator,
                         base_args.brain_url)
